6652fc315e37f76d456f7ae99b3794ccbe859306
sasming22156-netizen <sasming.22156@obhs.school.nz>
2026-03-19 09:32:05 +1300

    testing storing of data

--- form_handler.js
+++ form_handler.js
@@ -2,6 +2,8 @@ function myFunction(){ // Retrieving data
     var firstname = document.getElementById("firstname").value;
     var lastname = document.getElementById("lastname").value;
     var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var comment = document.getElementById("comment").value;
     var message = "Thanks for subscribing to our newsletter " + firstname + " " + lastname + ". We have sent an email to " + email + " to confirm your subscription.";
     var agree = document.getElementById("agree").checked;
     if (firstname === "" || lastname === "" || email === "" || agree === false) {
@@ -12,4 +14,6 @@ function myFunction(){ // Retrieving data
     var fname = localStorage.setItem("firstname",firstname) // Storing data
     var lname = localStorage.setItem("lastname", lastname)
     var em = localStorage.setItem("email",email)
+    var pn = localStorage.setItem("phone",phone)
+    var cmt = localStorage.setItem("comment",comment)
 }
