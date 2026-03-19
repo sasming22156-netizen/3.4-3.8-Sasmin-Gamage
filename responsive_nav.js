454c45d97440b4a34e96eb3fdfe359683522e4a4
sasming22156-netizen <sasming.22156@obhs.school.nz>
2026-03-20 09:05:01 +1300

    trailling phone navigation view

---
+++ responsive_nav.js
@@ -0,0 +1,8 @@
+function myFunction() { // This is my responsive navigation changing into phone view
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
