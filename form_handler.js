556fc41dbbb2b3ced9c4876800919936ebc8c189
sasming22156-netizen <sasming.22156@obhs.school.nz>
2026-03-27 09:20:43 +1300

    making it only numbers are allowed in phone number

--- form_handler.js
+++ form_handler.js
@@ -4,6 +4,7 @@ function submitContactForm(event) { // Retrieving data
     var lastname = document.getElementById("lastname").value.trim();
     var email = document.getElementById("email").value.trim();
     var phone = document.getElementById("phone").value.trim();
+    var phonePattern = /^[0-9+\-\s()]+$/;
     var dob = document.getElementById("dob").value.trim();
     var comments = document.getElementById("message").value.trim();
     var message = "Thanks for subscribing to our newsletter " + firstname + " " + lastname + ". We have sent an email to " + email + " to confirm your subscription.";
@@ -11,7 +12,11 @@ function submitContactForm(event) { // Retrieving data
     if (firstname === "" || lastname === "" || email === "" || phone === "" ||
         dob === "" || agree === false) {
         alert("Please make sure you have entered your full name, email address and you agree to our terms of service.");
-    } else {
+    }
+    else if (!phonePattern.test(phone)) {
+        alert("Please enter a valid phone number (numbers only).");
+    }
+    else {
         alert(message);
     }
     var fname = localStorage.setItem("firstname",firstname) // Storing data
