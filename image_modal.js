ab0f27694da9aa024272c8e08e8bdf7de6a9f37f
sasming22156-netizen <sasming.22156@obhs.school.nz>
2026-05-06 21:24:29 +1200

    adding titles and alt tags

--- image_modal.js
+++ image_modal.js
@@ -3,6 +3,8 @@ document.querySelectorAll('.card img').forEach(image =>{
 	image.onclick = () =>{
 	    document.querySelector('.pop_up_image').style.display = 'block';
 	    document.querySelector('.pop_up_image img').src = image.getAttribute('src');
+        document.querySelector('.pop_up_image img').alt = image.getAttribute('alt');
+		document.querySelector('.pop_up_image img').title = image.getAttribute('title');
 	}
 })               
 document.querySelector('.pop_up_image span').onclick = () =>{
