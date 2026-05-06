bf1869d1a0aeca12199724482879e51877e1ca07
sasming22156-netizen <sasming.22156@obhs.school.nz>
2026-05-06 21:30:38 +1200

    cleaning up code again

--- image_modal.js
+++ image_modal.js
@@ -1,12 +1,12 @@
 /* Javascript and the code for the modal */
-document.querySelectorAll('.card img').forEach(image =>{
-	image.onclick = () =>{
-	    document.querySelector('.pop_up_image').style.display = 'block';
-	    document.querySelector('.pop_up_image img').src = image.getAttribute('src');
-        document.querySelector('.pop_up_image img').alt = image.getAttribute('alt');
+document.querySelectorAll('.card img').forEach(image => {
+	image.onclick = () => {
+		document.querySelector('.pop_up_image').style.display = 'block';
+		document.querySelector('.pop_up_image img').src = image.getAttribute('src');
+		document.querySelector('.pop_up_image img').alt = image.getAttribute('alt');
 		document.querySelector('.pop_up_image img').title = image.getAttribute('title');
 	}
-})               
-document.querySelector('.pop_up_image span').onclick = () =>{
-document.querySelector('.pop_up_image').style.display = 'none';
+})
+document.querySelector('.pop_up_image span').onclick = () => {
+	document.querySelector('.pop_up_image').style.display = 'none';
 }
